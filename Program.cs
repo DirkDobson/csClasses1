457aefefa39ba4f0bcf9abfc616850872ff2b803
Dirk Dobson <dirkvancouver@live.com>
2019-01-24 18:21:18 -0700

    person class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            System.Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Dirk");
         }
     }
 }
